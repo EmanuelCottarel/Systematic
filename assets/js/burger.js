56b771eaceb0b050fbf9419ef0910232eebcece5
EmanuelCottarel <103122152+EmanuelCottarel@users.noreply.github.com>
2022-10-16 16:41:31 +0200

    "Burger menu , banner, nav icons"

---
+++ assets/js/burger.js
@@ -0,0 +1,8 @@
+var burgerMenu = document.getElementById('burger-menu');
+
+var overlay = document.getElementById('menu');
+
+burgerMenu.addEventListener('click', function() {
+  this.classList.toggle("close");
+  overlay.classList.toggle("overlay");
+});
